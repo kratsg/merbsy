b9b73f375860a7feb49d7ab23f05fd2b8f723d40
Giordon Stark <kratsg@gmail.com>
2013-06-24 02:31:03 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merbsy::Application.config.secret_token = 'df1c190b8f0d8f8c62e7e0c00ec4f50bac1392346304bc4aaebab40d6c608928a1b625bc448fb308f04d42cbbdd3d0686082d4c84b0ee11c86fff29bf4009287'
